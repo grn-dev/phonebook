8698255195fed936749fc6c58f859ff820cee60b
mahmoud <Mahmoudsabzalii@Gmail.com>
2020-03-22 02:03:05 -0700

    02

---
+++ PHONEBOOK/PHONEBOOK.ENDPOINT.MVC/Models/AddNewPeople ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PHONEBOOK.ENDPOINT.MVC.Models
+{
+    public class AddNewPeople_ViewModel
+    {
+    }
+}
