f25cadd8e808b7b8297b4301cb25bf280cd0bc88
mahmoud <Mahmoudsabzalii@Gmail.com>
2020-03-27 03:14:08 -0700

    user add List

---
+++ PHONEBOOK/PHONEBOOK.ENDPOINT.MVC/Controllers/UserController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PHONEBOOK.ENDPOINT.MVC.Models;
+using PHONEBOOK.ENDPOINT.MVC.Models.AAA;
+
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PHONEBOOK.ENDPOINT.MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<Appuser> UserManager;
+        public UserController(UserManager<Appuser> user)
+        {
+            this.UserManager = user;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var user = UserManager.Users.Take(20).ToList();
+            return View(user);
+        }
+
+        public IActionResult Creat()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Creat(CreatUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Appuser user = new Appuser();
+                user.UserName = model.username;
+                // user.PasswordHash = model.username;
+                user.Email = model.Email;
+
+
+                //.resule وایمیسته تا نتیجه بیاد
+                var save = UserManager.CreateAsync(user, model.username).Result;
+
+                if (save.Succeeded) {
+                    return RedirectToAction("Index");
+                }
+                else {
+                    foreach (var item in save.Errors)
+                    {
+                        ModelState.AddModelError(item.Code,item.Description);
+                    }
+                }
+
+                
+                //repoditory or savind here
+
+            }
+            else {
+                
+                return View(model);
+            }
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ PHONEBOOK/PHONEBOOK.ENDPOINT.MVC/Models/CreatUserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PHONEBOOK.ENDPOINT.MVC.Models
+{
+    public class CreatUserViewModel
+    {
+        [Required]
+        [MaxLength(50)]
+        public string username { get; set; }
+
+        [Required]
+        [MaxLength(050)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string password { get; set; }
+        
+    }
+}
